758e9cb850a231b71431fd33152ad43fbe7b64d7
Riza Fahmi <rizafahmi@gmail.com>
2022-02-25 08:39:50 +0700

    Finish React first component

---
+++ 5-react/1-react-dgn-vite/src/App.tsx
@@ -0,0 +1,13 @@
+import Players from './Players';
+
+function App() {
+
+  return (
+    <>
+      <h1>Feedloop Leaderboard</h1>
+      <Players />
+    </>
+  )
+}
+
+export default App
---
+++ 5-react/1-react-dgn-vite/src/Players.tsx
@@ -0,0 +1,20 @@
+function Players() {
+    return (
+        <div>
+            <div>
+                <div>
+                    Fathurozak Buhari
+                </div>
+                <div>
+                    <div>
+                        <button>-</button>
+                        <span>30</span>
+                        <button>+</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Players
